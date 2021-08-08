3d1812d067f0e9021ff111fb089d79549e1fdaa0
ATS527 <truespiritz527@gmail.com>
2021-08-08 17:01:26 +0530

    Created a new command called Around the web

---
+++ src/commands/General/AroundTheWeb.ts
@@ -0,0 +1,44 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+
+
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'aroundTheWeb',
+            description: 'Checks the answer students type',
+            aliases: ['atw', 'web'],
+            category: 'general',
+            usage: `${client.config.prefix}aroundTheWeb`,
+            adminOnly: false
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        var message = "\"";
+        var isTrue = false;
+        for (var i = 1; i < M.args.length; i++) {
+            if((M.args[i] === "signed" || M.args[i] === "Signed" || M.args[i] === "sign") && (M.args[i+1] === "APK" || M.args[i+1] === "apk" || M.args[i+1] === "Apk")) {
+                isTrue = true;
+            }
+            message = message + " " + M.args[i];
+        }
+        if(message === "") {
+             message = "Your answer is empty!!";
+        } else if (isTrue){
+            message = message + "\"\n\nThis answer is correct and deserves an appreciation!!Wohoo";
+        } else {
+            message = "Sorry try again :)";
+        }
+        
+        return void (await M.reply(
+            message,
+            undefined,
+            undefined,
+            [M.sender.jid],
+        ))
+    }
+}
